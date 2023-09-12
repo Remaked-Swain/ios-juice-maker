264b55877bda69d74361c7ed2e119475f2feffd1
DongJun96 <djlee1201@naver.com>
2023-09-12 11:06:42 +0900

    [Feat]: 재고관리, 레시피 함수 추가

--- JuiceMaker/JuiceMaker/Model/FruitStore.swift
+++ JuiceMaker/JuiceMaker/Model/FruitStore.swift
@@ -10,14 +10,19 @@ import Foundation
 final class FruitStore {
     // Fruits Properties
     private var fruitsStorage: [FruitType:Int] = [
-        .strawberry : 10, .banana : 10, .kiwi : 10, .pineapple : 10, .mango : 10
+        .strawberry : 10, .banana : 10, .kiwi : 10, .mango : 10, .pineapple : 10
     ]
+    
+//    , .banana : 10, .kiwi : 10, .pineapple : 10, .mango : 10
     
     // 주문 받은 것대로 재고 빼기
     func receiveOrder(juiceType: JuiceType) {
-        switch juiceType {
-        case .strawberryJuice: break
-        }
+        let totalIng = makingJuice(juice: juiceType)
+        let firtIng = totalIng.0
+        let secondIng = totalIng.1
+        checkQuantity(firstIngfredient: firtIng, secondIngredient: secondIng)
+        
+        
     }
 }
 
@@ -25,6 +30,7 @@ final class FruitStore {
 extension FruitStore {
     @frozen enum JuiceType {
         case strawberryJuice
+        case strawberryBananaMixJuice
 //        case bananaJuice, kiwiJuice, pineappleJuice, strawberryBananaMixJuice, mangoJuice, mangoKiwiMixJuice
     }
     
@@ -40,11 +46,40 @@ extension FruitStore {
 
 // MARK: Private methods
 extension FruitStore {
-    private func checkQuantity(_ firstIngredient: Fruit, _ secondIngredient: Fruit) throws {
+    
+    //레시피 함수 (주스메뉴를 받아서 필요한 재료를 말해주는 함수)
+    private func makingJuice(juice: JuiceType) -> (Fruit, Fruit?) {
+        switch juice {
+        case .strawberryJuice: return (Fruit(fruitType: .strawberry, quantity: 16), nil)
         
+        case .strawberryBananaMixJuice: return (Fruit(fruitType: .strawberry, quantity: 10),Fruit(fruitType: .banana, quantity: 1))
+        }
     }
     
-    private func updateInventory(_ fruitType: FruitType, count: Int) {
+    //재고 관리 함수 과일 수량이 음료 제조 가능한지 알려주는 함수
+    private func checkQuantity(firstIngfredient: Fruit, secondIngredient: Fruit?) {
+        guard let ing = fruitsStorage[firstIngfredient.fruitType] else { return }
+        if secondIngredient != nil {
+            guard let ing2 = fruitsStorage[secondIngredient.fruitType] else { return }
+        }
+        
+        if let fruit2 = fruitsStorage[secondIngredient?.fruitType]{
+            let ing2 = fruit2.value
+        }
+           
+        if let ing2 = fruitsStorage[secondIngredient?.fruitType] {
+            
+        }
+        
+        if ing - firstIngfredient.quantity > 0 && Ing2 - firstIngfredient.quantity > 0 {
+            print("음료 제조 가능")
+            updateInventory(Fruit(fruitType: <#T##FruitType#>, quantity: <#T##Int#>), <#T##secondIngredient: Fruit?##Fruit?#>)
+        }else {
+            print("음료 제조 불가능")
+        }
+    }
+    
+    private func updateInventory(_ firstIngfredient: Fruit, _ secondIngredient: Fruit?) {
         
     }
 }
--- JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
+++ JuiceMaker/JuiceMaker/Model/JuiceMaker.swift
@@ -8,5 +8,6 @@ import Foundation
 
 // 쥬스 메이커 타입
 struct JuiceMaker {
+    private let fruitStore = FruitStore()
     
 }
